c94fb10595fab68d622217a4eff4ee811388cd6e
Adam Knight <adam@revolutionarysystems.co.uk>
2015-02-24 16:14:21 +0000

    Added grunt script

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  grunt.loadNpmTasks('grunt-haven');
+
+  // Tasks
+
+  grunt.registerTask('dist', ['haven:update']);
+
+  grunt.registerTask('deploy', ['haven:deploy']);
+  grunt.registerTask('ci', ['haven:deployOnly']);
+
+  // Default task(s).
+  grunt.registerTask('default', ['deploy']);
+
+};
